db93610cd835914e57364312eeef8fd85567c18f
Tpython5150 <129126224+Tpython5150@users.noreply.github.com>
2023-06-27 04:18:08 -0700

    week3day3

---
+++ whiteboards.py
@@ -0,0 +1,7 @@
+#How To Whiteboard
+##1. Read the problem out loud
+##2. Making #any assumption, ask claryinging questions. (you are establishing good habits)
+#3. coming up with the approach (drawing pictures) (make sure you dont leave us the viewers in the dust)
+	#- ideally, you can come up with a COUPLE solutions, evaluate their complexities/efficiency/then make your pick
+#4. Write out the code (this should actually take a small amount of time)
+#5. Debug / re-evaluate
